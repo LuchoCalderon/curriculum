d648db19b6d95902bc4f050c28b51c2fdfa58f5a
luchocalderon16 <luchocalderon16@gmail.com>
2023-07-10 19:03:40 -0500

    Update formulario

--- script.js
+++ script.js
@@ -58,7 +58,79 @@ function mostrarDetalles3(){
 }
 
 
+function saveContact(){
+    let nameC = document.getElementById("name");
+    let emailC = document.getElementById("email");
+    let messageC = document.getElementById("message");
 
+
+    let contact = {
+        name: nameC.value,
+        email: emailC.value,
+        message: messageC.value,
+
+    }
+    console.log(contact);
+
+    let url= "http://localhost:8000/api/contact";
+
+    let params= {
+        method:"POST",
+        headers:{
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(contact),
+    }
+    fetch(url, params).then(response =>{
+        console.log("response");
+        console.log(response.json());
+
+        if(response.status === 201){
+            alert("created successfully");
+        }else{
+            alert("error");
+        }
+    });
+
+    return true;
+}
+
+function getContact(){
+
+
+    let url = "localhost:8000/api/contact";
+    let params = {
+    method: "GET",
+    headers:{
+        "Content-Type": "application/json"
+    },
+    }
+
+    fetch(url,params).then((response) =>{
+        console.log(response);
+        console.log(response.json());
+    });
+
+
+
+    
+    url = "https://api.agify.io/?name=meelad";
+    params = {      
+    method: "GET",
+    headers:{
+        "Content-Type": "application/json"
+    },
+    }
+
+    fetch(url,params).then((response) =>{
+        console.log(response);
+        console.log(response.json());
+    });
+
+
+    return true;
+}
+/*
 const processForm = (event)=>{
 
     event.preventDefault();
@@ -202,4 +274,4 @@ const loadBody = (event)=>{
 }
 
 
-
+*/
